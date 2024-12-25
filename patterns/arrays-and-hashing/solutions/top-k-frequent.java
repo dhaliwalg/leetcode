7b90ffb07013da5e944dca1c216961895553fcd4
Gurjit <dhaliwalgurjit022@gmail.com>
2024-12-25 01:05:50 -0500

    top k solution

--- patterns/arrays-and-hashing/solutions/top-k-frequent.java
+++ patterns/arrays-and-hashing/solutions/top-k-frequent.java
@@ -17,15 +17,63 @@
  * - -104 <= nums[i] <= 104
  * - k is in the range [1, the number of unique elements in the array]
  * - It is guaranteed that the answer is unique
- */
+ * 
+ * public class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
 
+        List<int[]> arr = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            arr.add(new int[] {entry.getValue(), entry.getKey()});
+        }
+        arr.sort((a, b) -> b[0] - a[0]);
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = arr.get(i)[1];
+        }
+        return res;
+    }
+}
+ */
+import java.util.*;
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // Initialize result array
-        int[] result = new int[k];
+        Map<Integer, Integer> resultMap = new HashMap<>();
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
+
+        for (int i = 0; i < bucket.length; i++) {
+            bucket[i] = new ArrayList<>();
+        }
+
+        for(int num: nums){
+            resultMap.put(num, resultMap.getOrDefault(num, 0)+1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry: resultMap.entrySet()){
+            int num = entry.getKey();
+            int freq = entry.getValue();
+            bucket[freq].add(num);
+        }
+
+        int[] res = new int[k];
+        int index = 0;
+        //Create result array, then mark the location in the result
+        //with index. Start at the end w/ most freq element - go til index = k
+        //go thru the bucket at the spot, add it to the result array
+        //++ is postfix operator so it'll start at 0
         
-        // TODO: Implement the solution
-        
-        return result;
+        for (int i = bucket.length - 1; i > 0 && index < k; i--) {
+            for (int n : bucket[i]) {
+                res[index++] = n;
+                if (index == k) {
+                    return res;
+                }
+            }
+        }
+        return res;
     }
 }
